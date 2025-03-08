236659629863963c2a380eaa014c51119528b9c8
Emir ömer <iemirrekt@gmail.com>
2025-03-08 21:14:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,56 @@
 // https://discord.com/api/v9/invites/{self.vanity_url}?with_counts=true&with_expiration=true
 
 const axios = require("axios");
+const { vanity_url, guild_id, token } = require("./config");
+const { chrome } = require('user-agents-generator');
 
-async function checkVanityUrl(url) {
-    axios({
-        method: "get",
-        url: `https://discord.com/api/v9/invites/${url}?with_counts=true&with_expiration=true`,
-    }).then((response) => {
-        console.log(response.data);
-    }).catch((error) => {
-        console.error(error);
-    });
+class Sniper {
+    constructor() {
+        this.vanity_url = vanity_url;      
+        this.guild_id = guild_id;
+        this.token = token;
+
+        this.headers = {"authorization": this.token, "user-agent": chrome()};
+        
+        this.payload = {"code": this.vanity_url};
+    }
+
+    checkVanityUrl(url) {
+        axios({
+            method: "get",
+            url: `https://discord.com/api/v9/invites/${url}?with_counts=true&with_expiration=true`
+        }).then((response) => {
+            if(response.data) {
+                return false
+            }
+        }).catch(() => {
+            return true
+        });
+    }
+
+    request(url) {
+        axios({
+            method: "post",
+            url: `https://discord.com/api/v9/guilds/${url}/vanity-url`,
+            headers: this.headers,
+            data: this.payload
+        }).then((response) => {
+            if(response.data) {
+                console.log(response.data)
+            }
+        }).catch((error) => {
+            console.log(error)
+        });
+    }
+
+    start() {
+        if (this.checkVanityUrl(this.vanity_url)) {
+            this.request(this.vanity_url)
+        }
+    }
 }
+
+setInterval(async () => {
+    await Sniper.start()
+}, 5000);
+
